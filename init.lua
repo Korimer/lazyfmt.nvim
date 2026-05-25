512c03459f7eb22deb765186619039c833e8a762
Korimer <thekclov@gmail.com>
2026-05-25 10:35:52 -0600

    simple baseline

---
+++ init.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+local nix_cmd = nil
+function M.detect_nix()
+  nix_cmd = "nix-shell -p"
+end
+
+local datadir = nil
+function M.create_data_dir()
+  datadir = vim.fn.stdpath("data") .. "/lazyfmt"
+end
+
+function M.write_shortcut(pkgname)
+  if datadir == nil then M.create_data_dir() end
+  if nix_cmd == nil then M.detect_nix() end
+  local shortcut = io.open(datadir .. "/" .. pkgname, "w")
+  shortcut.write(
+    "#!/usr/bin/env bash\n" .. nix_cmd .. " " .. pkgname .. " --run " .. [[\"\$@\""]]
+  )
+end
